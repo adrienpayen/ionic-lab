75a950421645dc6f4e71c53a5c682cec7cbe467a
Nouve Yann <nouve.yann@gmail.com>
2018-06-08 17:29:42 +0200

    Add provider into the provjetY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,27 +8,34 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {LaunchListPage} from "../pages/launch-list/launch-list";
+import {HttpClientModule} from "@angular/common/http";
+import {SpacexApiProvider} from "../providers/spacex-api/spacex-api";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
+    LaunchListPage,
     ListPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
+    LaunchListPage,
     ListPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SpacexApiProvider
   ]
 })
 export class AppModule {}
