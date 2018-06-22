ed74b54b061dce9662bbcd80a07c4d3ae23d0dcf
sam <samuelantunes@hotmail.fr>
2018-06-23 00:32:52 +0200

    Package not installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { CountdownModule } from 'ngx-countdown';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -19,8 +18,7 @@ import { SpacexApiProvider } from "../providers/spacex-api/spacex-api";
   imports: [
     BrowserModule,
     HttpClientModule,
-    IonicModule.forRoot(MyApp),
-    CountdownModule
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
