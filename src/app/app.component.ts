7082f89cd1c4f6c9ce4dd960db4eed063884b5fe
Adrien PAYEN <adrien.payen2@gmail.com>
2018-07-03 23:51:58 +0200

    re-design application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,14 +24,14 @@ export class MyApp {
       { title: 'Home', component: HomePage },
       { title: 'Launch', component: LaunchPage }
     ];
-
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
+      
+      this.statusBar.styleLightContent();
       this.splashScreen.hide();
     });
   }
